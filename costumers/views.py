957251edb4339cec75632c301f3b40677e2ae25e
Jakeline Martines <jakeline_13_martines@hotmail.com>
2024-07-26 01:57:05 +0000

    Check for existing customer by phone number and return pre-filled form if found

--- costumers/views.py
+++ costumers/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Costumers, Cars
+import re
 
 def costumers(request):
     if request.method == "GET":
@@ -13,12 +15,28 @@ def costumers(request):
         reg_number = request.POST.getlist('reg_number')
         year = request.POST.getlist('year')
         
+        filtered_customers = Costumers.objects.filter(phone=phone)
         
-        print(firstname)
-        print(car)
-        print(reg_number)
-        print(year)
+        if filtered_customers.exists():
+            return render(request, 'costumers.html', {'name': firstname, 'surname': lastname, 'phone': phone, 'email': email, 'cars':zip(car, reg_number, year)})
+        
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
+            return render(request, 'costumers.html', {'name': firstname, 'surname': lastname, 'phone': phone, 'cars':zip(car, reg_number, year)})
+    
+        filtered_customers = Costumers(
+            firstname = firstname,
+            lastname = lastname,
+            email = email,
+            phone = phone
+        )
+        
+        #filtered_customers.save()
+        
+        x = list(zip(car, reg_number, year))
+        
+        print(x)
         
         return HttpResponse('teste')
+
 
         
